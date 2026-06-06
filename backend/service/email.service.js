e1ea658556f763938421c8a91892504df0ddad3e
Mike Carter <carter71404@icloud.com>
2026-06-06 01:44:37 -0400

    left off working on the email template

--- backend/service/email.service.js
+++ backend/service/email.service.js
@@ -1,4 +1,7 @@
 import { Resend } from 'resend';
+import { renderToStaticMarkup } from 'react-dom/server';	
+import EmailTemplate from "../../frontend/src/pages/emailTemplate.jsx";
+import { AppError } from '../error/error.handler.js';
 import db from "../config/postgresql.config.js";
 
 const resend = new Resend(process.env.RESEND_KEY)
@@ -10,30 +13,26 @@ const resend = new Resend(process.env.RESEND_KEY)
     // Once the endpoint is triggered the token is then split to get it's payload
     // The payload contents (user.id, and customer email) will then be used to query to change the status of the customers quote
     // The link will the redirect the user to a blank page thanking them for acceptance of the quote
-    // 
+    //
+
+
+
 	export async function sendEmail(user, quoteData, link){
-        if(!user){
-            throw new Error("Invalid user.");
-        }
-        if(!quoteData){
-            throw new Error("Quote data is not provided cannot send empty quote.");
-        }
+        if(!quoteData) throw new AppError("Failed to provide quote data. Cannot send empty quote.", 400);
+		const html = renderToStaticMarkup(<EmailTemplate quoteInfo={quoteData} userInfo={user} link={link} />)
         try{
-            console.log(quoteData);
-            const { data, error } = await resend.emails.send({
+			const { data, error } = await resend.emails.send({
                 from: user.email,
                 to: quoteData.customer.email,
-                subject: `Quote from ${user.email}`,
-                html: `${quoteData}`,
-                html: `<a href="${link}"> Click the link to accept your quote!`
-                
+                subject: 'Quote',
+               	html 
             });
             if(error){
                 throw new Error(error.message);
             }
             console.log(data);
         }catch(err){
-            throw new Error(err.message);
+            throw err;
         }
     }
 
